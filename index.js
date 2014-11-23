3fc4bd1af2929875a6adf4d360da4bdecfa018c0
David Hayes <david@blackbricksoftware.com>
2014-11-23 11:58:50 -0600

    finish setup of all basics for web app and get it working

--- index.js
+++ index.js
@@ -1,7 +1,46 @@
-var server = require('http').createServer();
-var io = require('socket.io')(server);
-io.on('connection', function(socket){
-  socket.on('event', function(data){});
-  socket.on('disconnect', function(){});
+var	express = require('express'),
+	app = express(),
+	io = require('socket.io'),
+	fs = require('fs'),
+	request = require('request');
+	
+// serve static content
+app.use(express.static(__dirname + '/public'));
+
+// server up main page
+app.get('/',function(req,res){
+	fs.readFile(__dirname + '/index.html',function(err,data){
+		if (err) {
+			res.writeHead(500);
+			return res.end('Error loading index.html');
+		}
+		res.writeHead(200);
+		res.end(data);
+	});
 });
-server.listen(3000);
+
+// setup server
+var server = app.listen(1337);
+var socket = io.listen(server);
+
+// creating a new websocket to keep the content updated without any AJAX request
+socket.sockets.on('connection',function(socket){
+	console.log('A socket connected!');
+	getPrices();
+});
+
+getPrices();
+function getPrices(){
+	request.get('http://www.xmlcharts.com/cache/precious-metals.php?format=json&currency=usd', function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			//~ console.log(response.body);
+			try {
+				//~ var prices = JSON.parse(response.body);
+				socket.emit('prices',response.body);
+				//~ console.log(prices);
+			} catch(e) {
+				//~ console.log(e);
+			}
+		}
+	});
+}
